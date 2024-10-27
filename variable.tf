09c98b68778382c2782ab3b06e447b63ffe14c46
Uduak Promise <uduak7240@gmail.com>
2024-10-27 16:26:07 +0100

    first commit

---
+++ variable.tf
@@ -0,0 +1,13 @@
+#envirinmant variables
+variable "Region" {}
+variable "Project_Name" {}
+variable "Environment" {}
+
+#Vpc Variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
